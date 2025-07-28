ad5a55d00710bde99d1329f481f15cfe2a96ba4d
Alex Linden <alextl2004@gmail.com>
2025-07-28 15:08:31 -0700

    refactor authContext to avoid fast refresh issue

---
+++ pokemon-ui/src/context/AuthProvider.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import { AuthContext } from "./AuthContext";
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [token, setToken] = useState(null);
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem("user");
+        const storedToken = localStorage.getItem("token");
+
+        if (storedUser && storedToken) {
+            setUser(JSON.parse(storedUser));
+            setToken(storedToken);
+        }
+    }, []);
+
+    const login = ({ user, token }) => {
+        setUser(user);
+        setToken(token);
+        localStorage.setItem("user", JSON.stringify(user));
+        localStorage.setItem("token", token);
+    };
+
+    const logout = () => {
+        setUser(null);
+        setToken(null);
+        localStorage.removeItem("user");
+        localStorage.removeItem("token");
+    };
+
+    const isLoggedIn = !!user && !!token;
+
+    return (
+        <AuthContext.Provider value={{ user, token, isLoggedIn, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
