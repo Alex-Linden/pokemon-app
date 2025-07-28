7a75bf6339c50e30ddccab397a76d8309b92f0a9
Alex Linden <alextl2004@gmail.com>
2025-07-28 16:07:55 -0700

    fix register page import after refactor

--- pokemon-ui/src/pages/RegisterPage.jsx
+++ pokemon-ui/src/pages/RegisterPage.jsx
@@ -8,7 +8,7 @@ import {
   Stack,
 } from "@mui/material";
 import { registerUser } from "../services/auth";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/useAuth";
 import { useNavigate } from "react-router-dom";
 
 export default function RegisterPage() {
