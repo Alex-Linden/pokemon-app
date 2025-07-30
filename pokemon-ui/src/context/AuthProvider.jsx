d6538318b7580afc2e1faaf4ad674459952ae33f
Alex Linden <alextl2004@gmail.com>
2025-07-30 16:41:33 -0700

    WIP: adding caught to context

--- pokemon-ui/src/context/AuthProvider.jsx
+++ pokemon-ui/src/context/AuthProvider.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import { AuthContext } from "./AuthContext";
+import { getCaughtPokemon } from "../services/pokemon";
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [token, setToken] = useState(null);
+    const [caught, setCaught] = useState([]);
 
     useEffect(() => {
         const storedUser = localStorage.getItem("user");
@@ -15,11 +17,19 @@ export const AuthProvider = ({ children }) => {
         }
     }, []);
 
-    const login = ({ user, token }) => {
+    const login = async ({ user, token }) => {
         setUser(user);
         setToken(token);
         localStorage.setItem("user", JSON.stringify(user));
         localStorage.setItem("token", token);
+        try {
+            const caught = await getCaughtPokemon();
+            setCaught(caught);
+        } catch (e) {
+            console.error("❌ Error fetching caught Pokémon:", e);
+            setCaught([]);
+        }
+
     };
 
     const logout = () => {
@@ -32,7 +42,7 @@ export const AuthProvider = ({ children }) => {
     const isLoggedIn = !!user && !!token;
 
     return (
-        <AuthContext.Provider value={{ user, token, isLoggedIn, login, logout }}>
+        <AuthContext.Provider value={{ user, token, isLoggedIn, login, logout, caught, setCaught }}>
             {children}
         </AuthContext.Provider>
     );
